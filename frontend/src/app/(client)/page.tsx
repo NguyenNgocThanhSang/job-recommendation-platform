2e8d9916df47dfdf0459833f3cd7897c0ce2443b
Your Name <tlakhoa1803@gmail.com>
2024-12-21 20:39:44 +0700

    css pbi

--- frontend/src/app/(client)/page.tsx
+++ frontend/src/app/(client)/page.tsx
@@ -13,17 +13,31 @@ export default function Home() {
   }, []);
 
   const url =
-    'https://app.powerbi.com/reportEmbed?reportId=8ae8e760-1471-495f-a032-dc2e9bfd8ec7&autoAuth=true&ctid=2dff09ac-2b3b-4182-9953-2b548e0d0b39';
+    'https://app.powerbi.com/reportEmbed?reportId=8ae8e760-1471-495f-a032-dc2e9bfd8ec7&autoAuth=true&ctid=2dff09ac-2b3b-4182-9953-2b548e0d0b39&filterPaneEnabled=false&navContentPaneEnabled=false';
+
   return (
     <div
       style={{
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-        height: '100vh',
+        height: '90vh',
+        width: '90vw',
+        overflow: 'hidden', // Ẩn mọi phần nội dung thừa
+        padding: 0,
+        marginLeft:'60px'
       }}
     >
-      <iframe title="Tableau Dashboard" width="80%" height="100%" src={url} />
+      <iframe
+        title="Power BI Dashboard"
+        style={{
+          border: 'none',
+          width: '95%', // Đảm bảo vừa khung ngang
+          height: '95%', // Đảm bảo vừa khung dọc
+          overflow: 'hidden',
+        }}
+        src={url}
+      />
     </div>
   );
 }
